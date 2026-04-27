f64b0f0cee0b02a8de32fa7064ac92285683d0b9
Nicolas <nicolasroumec@hotmail.com>
2026-04-27 16:40:57 -0300

    chore: add initial database migration and entity fixes

--- TheShed.Shared/Models/Entities/PasswordEntryTag.cs
+++ TheShed.Shared/Models/Entities/PasswordEntryTag.cs
@@ -8,7 +8,7 @@ namespace TheShed.Shared.Models.Entities
         public int PasswordEntryId { get; set; }
         public int TagId { get; set; }
 
-        public PasswordEntry PasswordEntry { get; set; } = null!;
+        public PasswordEntry? PasswordEntry { get; set; }
         public Tag Tag { get; set; } = null!;
     }
 }
